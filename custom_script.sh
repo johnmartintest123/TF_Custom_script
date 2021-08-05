ef7e1549a787cf788a2b03f12e4641e644bec890
johnmartintest123 <80247829+johnmartintest123@users.noreply.github.com>
2021-08-05 14:36:08 +0530

    Add files via upload

---
+++ custom_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this script does the following:
+# Creates a user
+# Gives the user admin privileges
+# Removes the user's Password
+
+username=$1
+public_key=$2
+
+sudo adduser $username
+
+#sudo setfacl -Rm u:rootuser:rwx /home/$username
+
+sudo mkdir /home/$username/.ssh
+
+sudo setfacl -Rm u:rootuser:rwx /home/$username
+
+sudo echo $public_key > /home/$username/.ssh/authorized_keys
+
+sudo chmod 700 /home/$username/.ssh
+
+sudo chmod 600 /home/$username/.ssh/authorized_keys
+
+sudo chown -R $username:$username /home/$username
+
+sudo setfacl -Rb /home/$username
